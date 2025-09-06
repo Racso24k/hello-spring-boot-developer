c4eae4f2907fb2daa6d7fdcbe38e1948c62ebc44
Racso24k <u20211g192@upc.edu.pe>
2025-09-06 15:51:19 -0500

    chore: default setup.

---
+++ sabado/hello-spring-boot-development/src/main/java/acme/com/spring/boot/platform/HelloSpringBootDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package acme.com.spring.boot.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootDevelopmentApplication.class, args);
+    }
+
+}
---
+++ sabado/hello-spring-boot-development/src/test/java/acme/com/spring/boot/platform/HelloSpringBootDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package acme.com.spring.boot.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
